0c198eb63a1d951e859797101e118303129d85cb
Greg Black <gjblack2001@gmail.com>
2025-04-16 14:12:44 +0100

    Added sorting/filtering for users

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -19,12 +19,15 @@ router.get('/classes', (req, res) => {
       return res.status(500).send("Could not load class info");
     }
 
-    // Debug log all courses
-    console.log('All courses from database:', JSON.stringify(courses, null, 2));
+    // Extract unique instructors and locations for filters
+    const instructors = [...new Set(courses.map(course => course.instructor))];
+    const locations = [...new Set(courses.map(course => course.location).filter(Boolean))];
 
     res.render('classes', {
       title: 'Current Classes',
       courses: courses,
+      instructors: instructors,
+      locations: locations,
       user: req.session.user,
       isAdmin: req.session.user?.role === 'admin',
       csrfToken: req.csrfToken()
